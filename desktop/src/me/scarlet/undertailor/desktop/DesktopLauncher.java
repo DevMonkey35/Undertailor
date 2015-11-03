ded89b96635d2cbf34ecda4a2008712c29bc30ca
Xemiru Sanzenin <xemiruk@gmail.com>
2015-11-03 00:56:54 -0700

    Start of a new project.

---
+++ desktop/src/me/scarlet/undertailor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.scarlet.undertailor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.scarlet.undertailor.Undertailor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Undertailor(), config);
+	}
+}
