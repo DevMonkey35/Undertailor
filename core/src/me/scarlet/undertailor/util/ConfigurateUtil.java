bbce1a617631a8b23fd132e872cf5c89fa942522
Xemiru Sanzenin <xemiruk@gmail.com>
2015-11-12 02:18:43 -0700

    Make this utility more useful and actually let missing values be missed.

--- core/src/me/scarlet/undertailor/util/ConfigurateUtil.java
+++ core/src/me/scarlet/undertailor/util/ConfigurateUtil.java
@@ -1,45 +1,83 @@
 package me.scarlet.undertailor.util;
 
+import static me.scarlet.undertailor.Undertailor.debug;
+
+import me.scarlet.undertailor.exception.ConfigurationException;
+import me.scarlet.undertailor.exception.NoRecordedValueException;
 import ninja.leaping.configurate.ConfigurationNode;
 
 public class ConfigurateUtil {
     
-    // TODO replace runtime ex
-    public static int processInt(ConfigurationNode node) throws RuntimeException {
+    public static int processInt(ConfigurationNode node, Integer defaultt) {
         if(node.isVirtual()) {
-            throw new RuntimeException("value for node " + pathFromArray(node.getPath()) + " not present");
+            if(defaultt == null) {
+                throw new NoRecordedValueException("value for node " + pathFromArray(node.getPath()) + " not present");
+            } else {
+                return defaultt;
+            }
         } else {
             String str = node.getString();
             try {
-                return Integer.parseInt(str);
+                Integer returned = Integer.parseInt(str);
+                debug("configutil", "int retrieve at " + pathFromArray(node.getPath()) + " returned " + returned);
+                return returned;
             } catch(NumberFormatException e) {
-                throw new RuntimeException("bad value (\"" + str + "\") for node " + pathFromArray(node.getPath()));
+                throw new ConfigurationException("bad value (\"" + str + "\") for node " + pathFromArray(node.getPath()));
             }
         }
     }
     
-    public static int[] processIntArray(ConfigurationNode node) throws RuntimeException {
+    public static float processFloat(ConfigurationNode node, Float defaultt) {
         if(node.isVirtual()) {
-            throw new RuntimeException("value for node " + pathFromArray(node.getPath()) + " not present");
+            if(defaultt == null) {
+                throw new NoRecordedValueException("value for node " + pathFromArray(node.getPath()) + " not present");
+            } else {
+                return defaultt;
+            }
         } else {
             String str = node.getString();
             try {
-                return toPrimitiveArray(node.getList(obj -> {
+                Float returned = Float.parseFloat(str);
+                debug("configutil", "float retrieve at " + pathFromArray(node.getPath()) + " returned " + returned);
+                return returned;
+            } catch(NumberFormatException e) {
+                throw new ConfigurationException("bad value (\"" + str + "\") for node " + pathFromArray(node.getPath()));
+            }
+        }
+    }
+    
+    public static int[] processIntArray(ConfigurationNode node, Integer[] defaultt) {
+        if(node.isVirtual()) {
+            if(defaultt == null) {
+                throw new NoRecordedValueException("value for node " + pathFromArray(node.getPath()) + " not present");
+            } else {
+                return toPrimitiveArray(defaultt);
+            }
+        } else {
+            String str = node.getString();
+            try {
+                Integer[] returned = node.getList(obj -> {
                     return (int) Integer.parseInt(obj.toString());
-                }).toArray(new Integer[0]));
+                }).toArray(new Integer[0]);
+                debug("configutil", "intarray retrieve at " + pathFromArray(node.getPath()) + " returned " + returned.toString());
+                return toPrimitiveArray(returned);
             } catch(NumberFormatException e) {
-                throw new RuntimeException("bad value (\"" + str + "\") for node " + pathFromArray(node.getPath()));
+                throw new ConfigurationException("bad value (\"" + str + "\") for node " + pathFromArray(node.getPath()));
             }
         }
     }
     
-    public static String processString(ConfigurationNode node) throws RuntimeException {
+    public static String processString(ConfigurationNode node, String defaultt) {
         String str = node.getString(null);
         if(node.isVirtual() || str == null) {
-            throw new RuntimeException("value for node " + pathFromArray(node.getPath()) + " not present");
+            if(defaultt == null) {
+                throw new NoRecordedValueException("value for node " + pathFromArray(node.getPath()) + " not present");
+            } else {
+                return defaultt;
+            }
         }
         
-        System.out.println("returned: " + str);
+        debug("configutil", "intarray retrieve at " + pathFromArray(node.getPath()) + " returned " + str);
         return str;
     }
     
