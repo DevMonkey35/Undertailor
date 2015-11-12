dd7ec68d56e88ed06c81632f6564712efd82f440
Xemiru Sanzenin <xemiruk@gmail.com>
2015-11-12 02:19:07 -0700

    Sprite and Spritesheet.

---
+++ core/src/me/scarlet/undertailor/gfx/Sprite.java
@@ -0,0 +1,96 @@
+package me.scarlet.undertailor.gfx;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Sprite {
+    
+    public static class SpriteMeta {
+        
+        public static final String[] META_VALUES = {"originX", "originY", "wrapX", "wrapY", "offX", "offY"};
+        
+        public float originX, originY;
+        public int wrapX, wrapY, offX, offY;
+        
+        public SpriteMeta() {
+            this(0.0F, 0.0F, 0, 0, 0, 0);
+        }
+        
+        public SpriteMeta(float originX, float originY, int offX, int offY, int wrapX, int wrapY) {
+            this.originX = originX;
+            this.originY = originY;
+            this.wrapX = wrapX;
+            this.wrapY = wrapY;
+            this.offX = offX;
+            this.offY = offY;
+        }
+        
+        @Override
+        public String toString() {
+            return "[" + originX + ", " + originY + ", " + wrapX + ", " + wrapY + ", " + offX + ", " + offY + "]";
+        }
+    }
+    
+    private SpriteMeta meta;
+    private TextureRegion region;
+    public Sprite(TextureRegion sprite, SpriteMeta meta) {
+        this.region = sprite;
+        this.meta = meta;
+    }
+    
+    public TextureRegion getTextureRegion() {
+        return region;
+    }
+    
+    public SpriteMeta getMeta() {
+        return meta;
+    }
+    
+    public void draw(Batch batch, float posX, float posY) {
+        this.draw(batch, posX, posY, 1.0F);
+    }
+    
+    public void draw(Batch batch, float posX, float posY, float scale) {
+        this.draw(batch, posX, posY, scale, scale);
+    }
+    
+    public void draw(Batch batch, float posX, float posY, float scaleX, float scaleY) {
+        this.draw(batch, posX, posY, scaleX, scaleY, 0F);
+    }
+    
+    public void draw(Batch batch, float posX, float posY, float scaleX, float scaleY, float rotation) {
+        this.draw(batch, posX, posY, scaleX, scaleY, rotation, false);
+    }
+    
+    // assumes Batch.begin() was already called
+    public void draw(Batch batch, float posX, float posY, float scaleX, float scaleY, float rotation, boolean flipX) {
+        this.draw(batch, posX, posY, scaleX, scaleY, rotation, flipX, false);
+    }
+    
+    public void draw(Batch batch, float posX, float posY, float scaleX, float scaleY, float rotation, boolean flipX, boolean flipY) {
+        this.draw(batch, posX, posY, scaleX, scaleY, rotation, flipX, flipY, false);
+    }
+    
+    public void draw(Batch batch, float posX, float posY, float scaleX, float scaleY, float rotation, boolean flipX, boolean flipY, boolean ensureBottomLeft) { // for texts
+        float originX = 0, originY = 0;
+        int offX = 0, offY = 0;
+        if(meta != null) {
+            originX = meta.originX;
+            originY = meta.originY;
+            offX = meta.offX;
+            offY = meta.offY;
+        }
+        
+        float x = posX + (offX * scaleX);
+        float y = posY + (offY * scaleY);
+        if(ensureBottomLeft) {
+            x += (originX * scaleX);
+            y += (originY * scaleY);
+        }
+        
+        TextureRegion drawn = new TextureRegion(region);
+        drawn.flip(flipX, flipY);
+        
+        batch.draw(drawn, x, y, (originX * scaleX), (originY * scaleY), region.getRegionWidth(), region.getRegionHeight(), scaleX, scaleY, rotation);
+    }
+}
---
+++ core/src/me/scarlet/undertailor/gfx/SpriteSheet.java
@@ -0,0 +1,133 @@
+package me.scarlet.undertailor.gfx;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import me.scarlet.undertailor.Undertailor;
+import me.scarlet.undertailor.exception.ConfigurationException;
+import me.scarlet.undertailor.exception.TextureTilingException;
+import me.scarlet.undertailor.gfx.Sprite.SpriteMeta;
+import me.scarlet.undertailor.util.ConfigurateUtil;
+import ninja.leaping.configurate.ConfigurationNode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class SpriteSheet {
+    
+    public static class SpriteSheetMeta {
+        
+        public int gridX, gridY;
+        public SpriteMeta[] spriteMeta; // metadata per frame
+        
+        public SpriteSheetMeta() {
+            this.gridX = 1;
+            this.gridY = 1;
+            this.spriteMeta = null;
+        }
+    }
+    
+    public static SpriteSheet fromConfig(File folder, ConfigurationNode node) throws FileNotFoundException, TextureTilingException {
+        String name = node.getKey().toString();
+        SpriteSheetMeta meta = new SpriteSheetMeta();
+        FileHandle textureFile = Gdx.files.absolute(new File(folder, ConfigurateUtil.processString(node.getNode("file"), null)).getAbsolutePath());
+        if(!textureFile.exists()) {
+            throw new FileNotFoundException("Texture file didn't exist");
+        }
+        
+        Texture texture = new Texture(textureFile);
+        try {
+            meta.gridX = ConfigurateUtil.processInt(node.getNode("gridSizeX"), null);
+            meta.gridY = ConfigurateUtil.processInt(node.getNode("gridSizeY"), null);
+            ConfigurationNode metaRoot = node.getNode("spritemeta");
+            if(!metaRoot.isVirtual()) {
+                int spriteCount = meta.gridX * meta.gridY;
+                meta.spriteMeta = new SpriteMeta[spriteCount];
+                for(int i = 0; i < spriteCount; i++) {
+                    for(ConfigurationNode metaNode : metaRoot.getChildrenMap().values()) {
+                        if(metaNode.getKey().toString().contains("" + i)) {
+                            if(meta.spriteMeta[i] == null) {
+                                meta.spriteMeta[i] = new SpriteMeta();
+                            }
+                            
+                            SpriteMeta cmeta = meta.spriteMeta[i];
+                            try {
+                                cmeta.offX = ConfigurateUtil.processInt(metaNode.getNode("offX"), cmeta.offX);
+                                cmeta.offY = ConfigurateUtil.processInt(metaNode.getNode("offY"), cmeta.offY);
+                                cmeta.wrapX = ConfigurateUtil.processInt(metaNode.getNode("wrapX"), cmeta.wrapX);
+                                cmeta.wrapY = ConfigurateUtil.processInt(metaNode.getNode("wrapY"), cmeta.wrapY);
+                                cmeta.originX = ConfigurateUtil.processFloat(metaNode.getNode("originX"), cmeta.originX);
+                                cmeta.originY = ConfigurateUtil.processFloat(metaNode.getNode("originY"), cmeta.originY);
+                            } catch(Exception e) {
+                                e.printStackTrace();
+                            }
+                            
+                            Undertailor.debug("sheetman", "sheet " + name + " loaded sprite at index " + i + " with meta "
+                                    + meta.spriteMeta[i].toString());
+                        }
+                    }
+                }
+            }
+        } catch(ConfigurationException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        
+        return new SpriteSheet(name, texture, meta);
+    }
+    
+    private String sheetName;
+    private Sprite[] sprites;
+    public SpriteSheet(String sheetName, Texture image, SpriteSheetMeta meta) throws TextureTilingException {
+        this.sheetName = sheetName;
+        checkTexture(image, meta.gridX, meta.gridY);
+        sprites = new Sprite[meta.gridX * meta.gridY];
+        int spriteHeight = (int) (image.getHeight() / meta.gridY);
+        int spriteWidth = (int) (image.getWidth() / meta.gridX);
+        for(int iY = 0; iY < meta.gridY; iY++) {
+            for(int iX = 0; iX < meta.gridX; iX++) {
+                int pos = (iY * meta.gridX) + iX;
+                SpriteMeta smeta = null;
+                if(meta.spriteMeta != null && meta.spriteMeta[pos] != null) {
+                    smeta = meta.spriteMeta[pos];
+                }
+                
+                TextureRegion region = new TextureRegion(image);
+                int height = spriteHeight;
+                int width = spriteWidth;
+                int wrapY = 0;
+                if(smeta != null) {
+                    width -= smeta.wrapX;
+                    height -= smeta.wrapY;
+                    wrapY = smeta.wrapY;
+                }
+                
+                region.setRegion(iX * spriteWidth, (iY * spriteHeight) + wrapY, width, height);
+                sprites[pos] = new Sprite(region, smeta);
+            }
+        }
+    }
+    
+    public Sprite[] getSprites() {
+        return sprites;
+    }
+    
+    public Sprite getSprite(int index) {
+        return sprites[index];
+    }
+    
+    public String getSheetName() {
+        return sheetName;
+    }
+    
+    private void checkTexture(Texture image, int width, int height) throws TextureTilingException {
+        if(image.getWidth() % width != 0) {
+            throw new TextureTilingException("Texture width is not divisible by defined width");
+        }
+        
+        if(image.getHeight() % height != 0) {
+            throw new TextureTilingException("Texture width is not divisible by defined height");
+        }
+    }
+}
