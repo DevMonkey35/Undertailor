4606cc260cbd119ac6c80ef9d4cab3effd6117fa
Xemiru Sanzenin <xemiruk@gmail.com>
2016-01-04 21:17:06 -0700

    mother of licenses

--- core/src/me/scarlet/undertailor/Undertailor.java
+++ core/src/me/scarlet/undertailor/Undertailor.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  * 
- * Copyright (c) 2015 Marc Tellerva
+ * Copyright (c) 2016 Tellerva, Marc Lawrence
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
