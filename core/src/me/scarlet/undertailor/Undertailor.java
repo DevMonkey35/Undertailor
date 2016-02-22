6231a37bebbeefeebb3ce11a5a7e00214c059c8d
Xemiru Sanzenin <xemiruk@gmail.com>
2016-02-22 05:52:27 -0700

    Actually use the MultiRenderer's clear method.

--- core/src/me/scarlet/undertailor/Undertailor.java
+++ core/src/me/scarlet/undertailor/Undertailor.java
@@ -29,7 +29,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import javafx.beans.value.ObservableValue;
@@ -276,7 +275,7 @@ public class Undertailor extends ApplicationAdapter {
         Environment activeEnv = environmentManager.getActiveEnvironment();
         float delta = Gdx.graphics.getDeltaTime();
         
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Undertailor.getRenderer().clear();
         
         if(activeEnv != null) {
             activeEnv.render();
