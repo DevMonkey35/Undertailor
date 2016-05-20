ffafd088976e37ed4a7b8b225d795abf8a186a34
Xemiru Sanzenin <xemiruk@gmail.com>
2016-05-19 17:14:49 -0700

    Automagical resource dumping implementation.

--- core/src/me/scarlet/undertailor/resource/Resource.java
+++ core/src/me/scarlet/undertailor/resource/Resource.java
@@ -1,8 +1,32 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Tellerva, Marc Lawrence
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package me.scarlet.undertailor.resource;
 
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.TimeUtils;
 
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.WeakHashMap;
@@ -22,18 +46,28 @@ import java.util.WeakHashMap;
  * available, or will be made available upon making the
  * request. This class works in tandem with the
  * {@link ResourceHandler} class which will track and
- * dispose of any disposable objects deemed no longer in
- * active use, by the means of object lifetime or if a
- * referent has removed itself from the list of its
- * users.</p>
+ * dispose of any Resources no longer in active use, by the
+ * means of becoming weakly reachable as determined by the
+ * Java Virtual Machine or through the end of its lifetime
+ * after the last time it was accessed.</p>
+ * 
+ * <p>With the above noted, the underlying resource should
+ * <strong>never</strong> be directly stored by classes that
+ * wish to use the resource, and should instead always
+ * retrieve the resource through the use of this class, if
+ * the implementation permits it as opposed to being a proxy
+ * for the functionality of the former.</p>
  */
 public abstract class Resource<T extends Disposable> {
     
     static Map<Class<? extends Disposable>, Long> lifetimeMapping;
+    protected static Map<Disposable, WeakReference<Resource<?>>> livingResources;
+    
     public static final long DEFAULT_LIFETIME = 3000; // 3s
     
     static {
         Resource.lifetimeMapping = new HashMap<>();
+        Resource.livingResources = new WeakHashMap<>();
     }
     
     /**
@@ -79,12 +113,10 @@ public abstract class Resource<T extends Disposable> {
     
     private T disposable;
     private long lastAccessTime;
-    private Map<Object, Object> referrers; // Used more like a list/set than a map.
     
-    public Resource() {
+    protected Resource() {
         this.disposable = null;
         this.lastAccessTime = TimeUtils.millis();
-        this.referrers = new WeakHashMap<>();
     }
     
     /**
@@ -92,11 +124,11 @@ public abstract class Resource<T extends Disposable> {
      * or may not be null.
      * 
      * <p>Retrieving the resource through this method does
-     * not trigger a refresh of its access lifetime.</p>
+     * not trigger a refresh of its last access time.</p>
      * 
      * @return the currently held instance of the resource
      */
-    final T getRawReference() {
+    protected final T getRawReference() {
         return this.disposable;
     }
     
@@ -106,9 +138,9 @@ public abstract class Resource<T extends Disposable> {
      * 
      * @return an instance of the resource
      */
-    final T getReference() {
+    protected final T getReference() {
         if(this.disposable == null) {
-            this.disposable = this.newReference();
+            this.createReference();
         }
         
         this.lastAccessTime = TimeUtils.millis();
@@ -116,17 +148,35 @@ public abstract class Resource<T extends Disposable> {
     }
     
     /**
+     * Returns the count of milliseconds since the last time
+     * this resource was accessed.
+     * 
+     * @return how long its been since
+     *         {@link #getReference()} was called
+     */
+    protected final long getTimeSinceLastAccess() {
+        return TimeUtils.timeSinceMillis(this.lastAccessTime);
+    }
+    
+    /**
      * Disposes of the underlying resource, allowing the
      * system to reclaim the memory used by the former.
      */
     public final void dispose() {
+        this.onDispose();
         if(this.disposable != null) {
-            this.referrers.clear();
+            Resource.livingResources.remove(this.disposable);
             this.disposable.dispose();
             this.disposable = null;
             this.lastAccessTime = -1;
         }
     }
+    
+    /**
+     * Called right before the system disposes of the
+     * underlying resource.
+     */
+    protected abstract void onDispose();
     
     /**
      * Generates a new reference to the underlying resource.
@@ -137,7 +187,14 @@ public abstract class Resource<T extends Disposable> {
      * wrapper's first instantiation or after it has been
      * previously disposed.</p>
      */
-    abstract T newReference();
+    protected abstract T newReference();
+    
+    /**
+     * Returns the class type of the underlying resouce.
+     * 
+     * @return the class of the underlying disposable object
+     */
+    protected abstract Class<T> getResourceClass();
     
     /**
      * Queries whether or not the underlying resource is
@@ -156,9 +213,13 @@ public abstract class Resource<T extends Disposable> {
      * }
      * </pre>
      */
-    boolean isDisposable() {
+    protected boolean isDisposable() {
         return this.disposable != null
-            && this.referrers.isEmpty()
             && TimeUtils.timeSinceMillis(this.lastAccessTime) > Resource.getLifetimeForResource(disposable.getClass());
+    }
+    
+    private void createReference() {
+        this.disposable = this.newReference();
+        Resource.livingResources.put(this.disposable, new WeakReference<>(this));
     }
 }
--- core/src/me/scarlet/undertailor/resource/ResourceHandler.java
+++ core/src/me/scarlet/undertailor/resource/ResourceHandler.java
@@ -1,5 +1,86 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Tellerva, Marc Lawrence
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package me.scarlet.undertailor.resource;
 
-public class ResourceHandler {
+import com.badlogic.gdx.utils.Disposable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.ref.WeakReference;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * Thread working in parallel with the main thread in order
+ * to clean and dispose of {@link Resource}s no longer in
+ * use.
+ */
+public class ResourceHandler extends Thread {
+    
+    static Logger log = LoggerFactory.getLogger(ResourceHandler.class);
+    
+    private Set<Disposable> removedKeys;
+    
+    public ResourceHandler() {
+        this.setDaemon(true);
+        this.setName("ResourceHandler");
+        
+        this.removedKeys = new HashSet<>();
+    }
+    
+    @Override
+    public void run() {
+        log.info("Resource handler has been started.");
+        
+        while(true) {
+            this.removedKeys.clear();
+            Iterator<Entry<Disposable, WeakReference<Resource<?>>>> iterator = Resource.livingResources.entrySet().iterator();
+            while(iterator.hasNext()) {
+                Entry<Disposable, WeakReference<Resource<?>>> entry = iterator.next();
+                Resource<?> resource = entry.getValue().get();
+                if(resource == null || (resource.getTimeSinceLastAccess() > Resource.getLifetimeForResource(resource.getResourceClass())) && resource.isDisposable()) {
+                    log.info("Disposing resource with disposable type " + entry.getKey().getClass().getSimpleName() + ".");
+                    if(resource == null) {
+                        entry.getKey().dispose();
+                    } else {
+                        resource.dispose();
+                    }
+                    
+                    this.removedKeys.add(entry.getKey());
+                }
+            }
+            
+            synchronized(Resource.livingResources) {
+                this.removedKeys.forEach(Resource.livingResources::remove);
+            }
+            
+            try {
+                Thread.sleep(500);
+            } catch(InterruptedException ignored) {}
+        }
+    }
+    
 }
