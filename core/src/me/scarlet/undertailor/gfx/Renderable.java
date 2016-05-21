e683833539e0f5b77431fbacda1f46f1356b910a
Xemiru Sanzenin <xemiruk@gmail.com>
2016-05-20 20:04:10 -0700

    Better documentation for Renderable.

--- core/src/me/scarlet/undertailor/gfx/Renderable.java
+++ core/src/me/scarlet/undertailor/gfx/Renderable.java
@@ -4,23 +4,53 @@ package me.scarlet.undertailor.gfx;
  * Skeleton implementation of something that can be drawn.
  */
 public interface Renderable {
-    
+
+    /**
+     * @see #draw(float, float, float, float, boolean,
+     *      boolean, float)
+     */
     public default void draw(float x, float y) {
-        this.draw(x, y, 1F, 1F);
+        this.draw(x, y, 1F);
     }
-    
+
+    /**
+     * @see #draw(float, float, float, float, boolean,
+     *      boolean, float)
+     */
     public default void draw(float x, float y, float scale) {
         this.draw(x, y, scale, scale);
     }
-    
+
+    /**
+     * @see #draw(float, float, float, float, boolean,
+     *      boolean, float)
+     */
     public default void draw(float x, float y, float scaleX, float scaleY) {
         this.draw(x, y, scaleX, scaleY, false, false);
     }
-    
-    public default void draw(float x, float y, float scaleX, float scaleY, boolean flipX, boolean flipY) {
+
+    /**
+     * @see #draw(float, float, float, float, boolean,
+     *      boolean, float)
+     */
+    public default void draw(float x, float y, float scaleX, float scaleY, boolean flipX,
+        boolean flipY) {
         this.draw(x, y, scaleX, scaleY, flipX, flipY, 0F);
     }
-    
-    public void draw(float x, float y, float scaleX, float scaleY, boolean flipX, boolean flipY, float rotation);
-    
+
+    /**
+     * Draws this {@link Renderable} with the given
+     * parameters.
+     * 
+     * @param x the x position of the sprite's anchor point
+     * @param y the y position of the sprite's anchor point
+     * @param scaleX the scaling of the sprite horizontally
+     * @param scaleY the scaling of the sprite vertically
+     * @param rotation the rotation of the sprite, anchored
+     *        at the anchor point defined by the origin
+     *        offset values
+     */
+    public void draw(float x, float y, float scaleX, float scaleY, boolean flipX, boolean flipY,
+        float rotation);
+
 }
