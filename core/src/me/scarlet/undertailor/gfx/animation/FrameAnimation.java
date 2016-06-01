84a4952dd322363c2d228e4a715e6ab4bb969d1c
Xemiru Sanzenin <xemiruk@gmail.com>
2016-05-31 21:26:56 -0700

    Missing licenses.

--- core/src/me/scarlet/undertailor/gfx/animation/FrameAnimation.java
+++ core/src/me/scarlet/undertailor/gfx/animation/FrameAnimation.java
@@ -1,3 +1,33 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Tellerva, Marc Lawrence
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 package me.scarlet.undertailor.gfx.animation;
 
 import org.slf4j.Logger;
@@ -18,6 +48,8 @@ public class FrameAnimation extends Animation {
      * {@link FrameAnimation}.
      */
     public static class KeyFrame {
+
+        static final KeyFrameData DEFAULT_DATA = new KeyFrameData(0, 0);
 
         /**
          * Holds data of a {@link KeyFrame}, pertaining to
@@ -119,6 +151,10 @@ public class FrameAnimation extends Animation {
         private Renderable frame;
         private KeyFrameData data;
 
+        public KeyFrame(long time, Renderable frame) {
+            this(time, frame, null);
+        }
+
         public KeyFrame(long time, Renderable frame, KeyFrameData data) {
             if (frame instanceof Animation) {
                 throw new IllegalArgumentException("Cannot use another animation in a frame");
@@ -154,10 +190,14 @@ public class FrameAnimation extends Animation {
          * Returns the {@link KeyFrameData} associated with
          * this {@link KeyFrame}.
          * 
+         * <p>If no frame data is found, a default frame
+         * data object with default values is provided
+         * instead.</p>
+         * 
          * @return a KeyFrameData
          */
         public KeyFrameData getFrameData() {
-            return this.data;
+            return this.data == null ? FrameAnimation.KeyFrame.DEFAULT_DATA : this.data;
         }
     }
 
