c271abd35671b949bb6b07fe758c77138400ae1d
Xemiru Sanzenin <xemiruk@gmail.com>
2016-07-28 20:19:46 -0700

    Missing license and doc.

--- core/src/me/scarlet/undertailor/ErrorWindow.java
+++ core/src/me/scarlet/undertailor/ErrorWindow.java
@@ -1,3 +1,33 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Tellerva, Marc Lawrence
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 package me.scarlet.undertailor;
 
 import javafx.beans.value.ObservableValue;
@@ -24,13 +54,11 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+/**
+ * The error window that pops up when something goes wrong
+ * in the game.
+ */
 public class ErrorWindow extends Stage {
-
-    static String toString(Throwable thrown) {
-        StringWriter writer = new StringWriter();
-        thrown.printStackTrace(new PrintWriter(writer));
-        return writer.toString();
-    }
 
     public ErrorWindow(Throwable thrown) {
         AnchorPane parent = new AnchorPane();
@@ -43,7 +71,9 @@ public class ErrorWindow extends Stage {
         if (thrown == null) {
             errr.appendText("No Java stacktrace given.\n");
         } else {
-            errr.appendText(toString(thrown));
+            StringWriter writer = new StringWriter();
+            thrown.printStackTrace(new PrintWriter(writer));
+            errr.appendText(writer.toString());
         }
 
         Label last = new Label(
